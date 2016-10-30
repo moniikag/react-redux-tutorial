25f32e708a070f85f3389750e88f2e5ac2d1f577
monika glier <monikaglier@gmail.com>
2016-10-30 19:21:57 +0100

    turn TodoApp component into a function

--- src/index.js
+++ src/index.js
@@ -133,46 +133,47 @@ const getVisibleTodos = (
 }
 
 let nextTodoId = 0
-class TodoApp extends Component {
-  render() {
-    const { todos, visibilityFilter } = this.props
-    const visibleTodos = getVisibleTodos(
-      todos,
-      visibilityFilter
-    )
-    return (
-      <div>
-        <AddTodo
-          onAddClick={text =>
-            store.dispatch({
-              type: 'ADD_TODO',
-              id: nextTodoId++,
-              text
-            })
-          }
-        />
-        <TodoList
-          todos={visibleTodos}
-          onTodoClick={id =>
-            store.dispatch({
-              type: 'TOGGLE_TODO',
-              id
-            })
-          }
-        />
-      <Footer
-        visibilityFilter={visibilityFilter}
-        onFilterClick={filter =>
-          store.dispatch({
-            type: 'SET_VISIBILITY_FILTER',
-            filter
-          })
-        }
-      />
-      </div>
-    )
-  }
-}
+
+const TodoApp = ({
+  todos,
+  visibilityFilter
+}) => (
+  <div>
+    <AddTodo
+      onAddClick={text =>
+        store.dispatch({
+          type: 'ADD_TODO',
+          id: nextTodoId++,
+          text
+        })
+      }
+    />
+    <TodoList
+      todos={
+        getVisibleTodos(
+          todos,
+          visibilityFilter
+        )
+      }
+      onTodoClick={id =>
+        store.dispatch({
+          type: 'TOGGLE_TODO',
+          id
+        })
+      }
+    />
+  <Footer
+    visibilityFilter={visibilityFilter}
+    onFilterClick={filter =>
+      store.dispatch({
+        type: 'SET_VISIBILITY_FILTER',
+        filter
+      })
+    }
+  />
+  </div>
+)
+
 
 const render = () => {
   ReactDOM.render(
