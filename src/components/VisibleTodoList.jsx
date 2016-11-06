7c1b00275a1ee10ab6785a0a193d0f16346f6504
monika glier <monikaglier@gmail.com>
2016-11-06 21:17:48 +0100

    pass configuration object instead of mapDispatchToProps

--- src/components/VisibleTodoList.jsx
+++ src/components/VisibleTodoList.jsx
@@ -58,14 +58,10 @@ const mapStateToProps = (state, { params }) => ({
   )
 })
 
-const mapDispatchToProps = (dispatch) => ({
-  onTodoClick: (id) =>
-    dispatch(toggleTodo(id))
-})
-
 const VisibleTodoList = withRouter(connect(
   mapStateToProps,
-  mapDispatchToProps
+  // we can just pass a configuration object when payload is the same
+  { onTodoClick: toggleTodo }
 )(TodoList))
 
 export default VisibleTodoList
