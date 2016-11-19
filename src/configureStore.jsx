6b2fb173ec7cf30302ca988d9abf305750321b6a
monika glier <monikaglier@gmail.com>
2016-11-19 17:36:19 +0100

    use arrow functions syntax for middlewares

--- src/configureStore.jsx
+++ src/configureStore.jsx
@@ -1,53 +1,42 @@
 import { createStore } from 'redux'
 import todoApp from './reducers'
-import { loadState, saveState } from './localStorage'
 
-const logger = (store) => {
-  return (next) => {
-    const next = store.dispatch
-    if (!console.group) {
-      return next
-    }
+const logger = (store) => (next) => {
+  if (!console.group) {
+    return next
+  }
 
-    return (action) => {
-      console.group(action.type)
-      console.log('%c prev state', 'color: gray', store.getState())
-      console.log('%c action', 'color: blue', action)
-      const returnValue = rawDispatch(action)
-      console.log('%c next state', 'color: green', store.getState())
-      console.groupEnd(action.type)
-      return returnValue
-    }
+  return (action) => {
+    console.group(action.type)
+    console.log('%c prev state', 'color: gray', store.getState())
+    console.log('%c action', 'color: blue', action)
+    const returnValue = next(action)
+    console.log('%c next state', 'color: green', store.getState())
+    console.groupEnd(action.type)
+    return returnValue
   }
 }
 
-const promise = (store) => {
-  return (next) => {
-    const next = store.dispatch
-    return (action) => {
-      if (typeof action.then === 'function') {
-        return action.then(next)
-      }
-      return next(action)
-    }
+const promise = (store) => (next) => (action) => {
+  if (typeof action.then === 'function') {
+    return action.then(next)
   }
+  return next(action) // next dispatch function in the chain
 }
 
 const wrapDispatchWithMiddlewares = (store, middlewares) => {
-  middlewares.forEach(middleware =>
+  middlewares.slice().reverse().forEach(middleware =>
     store.dispatch = middleware(store)(store.dispatch)
   )
 }
 
 const configureStore =() => {
-  const store = createStore(todoApp, persistedState)
-  const middlewares = []
+  const store = createStore(todoApp)
+  const middlewares = [promise]
 
   if (process.env.NODE_ENV !== 'production') {
     middlewares.push(logger)
   }
-
-  middlewares.push(promise(store))
 
   wrapDispatchWithMiddlewares(store, middlewares)
 
