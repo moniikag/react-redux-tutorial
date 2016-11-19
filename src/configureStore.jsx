97d744c2a572daad57688639897edefba21fcac9
monika glier <monikaglier@gmail.com>
2016-11-19 17:18:11 +0100

    create function wrapping dispatch with middlewares

--- src/configureStore.jsx
+++ src/configureStore.jsx
@@ -1,54 +1,55 @@
 import { createStore } from 'redux'
-import throttle from 'lodash/throttle'
 import todoApp from './reducers'
 import { loadState, saveState } from './localStorage'
 
-const addLoggingToDispatch = (store) => {
-  const rawDispatch = store.dispatch
-  if (!console.group) {
-    return rawDispatch
-  }
+const logger = (store) => {
+  return (next) => {
+    const next = store.dispatch
+    if (!console.group) {
+      return next
+    }
 
-  return (action) => {
-    console.group(action.type)
-    console.log('%c prev state', 'color: gray', store.getState())
-    console.log('%c action', 'color: blue', action)
-    const returnValue = rawDispatch(action)
-    console.log('%c next state', 'color: green', store.getState())
-    console.groupEnd(action.type)
-    return returnValue
+    return (action) => {
+      console.group(action.type)
+      console.log('%c prev state', 'color: gray', store.getState())
+      console.log('%c action', 'color: blue', action)
+      const returnValue = rawDispatch(action)
+      console.log('%c next state', 'color: green', store.getState())
+      console.groupEnd(action.type)
+      return returnValue
+    }
   }
 }
 
-const addPromiseSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch
-  return (action) => {
-    if (typeof action.then === 'function') {
-      return action.then(rawDispatch)
+const promise = (store) => {
+  return (next) => {
+    const next = store.dispatch
+    return (action) => {
+      if (typeof action.then === 'function') {
+        return action.then(next)
+      }
+      return next(action)
     }
-    return rawDispatch(action)
   }
+}
+
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+  middlewares.forEach(middleware =>
+    store.dispatch = middleware(store)(store.dispatch)
+  )
 }
 
 const configureStore =() => {
-  const persistedState = loadState()
   const store = createStore(todoApp, persistedState)
-
-  store.dispatch = addPromiseSupportToDispatch(store)
+  const middlewares = []
 
   if (process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store)
+    middlewares.push(logger)
   }
 
+  middlewares.push(promise(store))
 
-  // save state on any store change
-  // save only data (todos), not the application state (filter)
-  // throttle ensures that the function is not called more often than specified in ms
-  store.subscribe(throttle(() => {
-    saveState({
-      todos: store.getState().todos
-    })
-  }, 1000))
+  wrapDispatchWithMiddlewares(store, middlewares)
 
   return store
 }
