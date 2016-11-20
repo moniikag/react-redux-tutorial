bae92b1556b640abc7ffbd0e26210932a838d3df
monika glier <monikaglier@gmail.com>
2016-11-20 20:00:16 +0100

    display error or retry button //24

--- src/api/index.js
+++ src/api/index.js
@@ -21,7 +21,9 @@ const delay = (ms) =>
 
 export const fetchTodos = (filter) =>
   delay(500).then(() => {
-    throw new Error('Boom!')
+    if (Math.random() > 0.5) {
+      throw new Error('Boom!')
+    }
 
     switch (filter) {
       case 'all':
--- src/components/VisibleTodoList.jsx
+++ src/components/VisibleTodoList.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import * as actions from '../actions'
-import { getVisibleTodos, getIsFetching } from '../reducers'
+import { getVisibleTodos, getIsFetching, getErrorMessage } from '../reducers'
+import FetchError from './FetchError'
 
 class VisibleTodoList extends Component {
   componentDidMount() {
@@ -21,9 +22,17 @@ class VisibleTodoList extends Component {
   }
 
   render() {
-    const { toggleTodo, todos, isFetching } = this.props
+    const { toggleTodo, errorMessage, todos, isFetching } = this.props
     if (isFetching && !todos.length) {
       return <p>Loading...</p>
+    }
+    if (errorMessage && !todos.length) {
+      return (
+        <FetchError
+          message={errorMessage}
+          onRetry={() => this.fetchData()}
+        />
+      )
     }
 
     return <TodoList
@@ -67,8 +76,9 @@ const TodoList = ({
 const mapStateToProps = (state, { params }) => {
   const filter = params.filter || 'all'
   return {
-    todos: getVisibleTodos(state, filter),
+    errorMessage: getErrorMessage(state, filter),
     isFetching: getIsFetching(state, filter),
+    todos: getVisibleTodos(state, filter),
     filter
   }
 }
